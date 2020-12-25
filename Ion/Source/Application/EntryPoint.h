5477fb57c54c99c1477deb3f98463b4509e5c8d7
Jacek <burnertski@gmail.com>
2020-12-25 05:17:17 +0100

    Update EntryPoint.h
    Added a pause message on close.

--- Ion/Source/Application/EntryPoint.h
+++ Ion/Source/Application/EntryPoint.h
@@ -9,6 +9,8 @@ int main(int argc, char** argv)
 	Ion::WindowsApplication* application = dynamic_cast<Ion::WindowsApplication*>(Ion::CreateApplication());
 	application->InitWindows(hInstance);
 	delete application;
+	ION_LOG_INFO("Press Enter to close.");
+	getchar();
 	return 0;
 }
 
