06b01839221e614e019e1520df7de138e5a56fc3
Jacek <burnertski@gmail.com>
2020-12-25 04:19:59 +0100

    Updated EventQueue
    EventQueue now uses references for event collection instead of shared pointers.

--- Ion/Source/Event/EventQueue.cpp
+++ Ion/Source/Event/EventQueue.cpp
@@ -12,18 +12,20 @@ namespace Ion
 	{
 		if (!m_Events.empty())
 		{
-			ION_LOG_ENGINE_TRACE("Processing events in queue.");
-
 			for (auto it = m_Events.begin(); it != m_Events.end(); ++it)
 			{
-				Event& event = **it;
-				ION_LOG_ENGINE_DEBUG(event.Debug_ToString());
-				//EventDispatcher dispatcher(event);
+				Event& event = *it;
+				m_Handler(event);
 			}
 
 			m_Events.clear();
 			return true;
 		}
 		return false;
+	}
+
+	void EventQueue::SetEventHandler(EventHandler handler)
+	{
+		m_Handler = handler;
 	}
 }
--- Ion/Source/Event/EventQueue.h
+++ Ion/Source/Event/EventQueue.h
@@ -1,21 +1,29 @@
 #pragma once
 
 #include "Event.h"
+#include <unordered_map>
 
 namespace Ion
 {
 	class ION_API EventQueue
 	{
+		using EventHandler = std::function<void(Event&)>;
+		using EventRef = std::reference_wrapper<Event>;
+
 	public:
 		template<typename EventT, typename std::enable_if<std::is_base_of<Event, EventT>::value>::type* = nullptr>
 		void PushEvent(EventT& event)
 		{
-			m_Events.push_back(std::make_shared<EventT>(event));
+			m_Events.push_back(event);
 		}
 
 		bool ProcessEvents();
 
+		void SetEventHandler(EventHandler handler);
+
 	private:
-		std::vector<std::shared_ptr<Event>> m_Events;
+		std::vector<EventRef> m_Events;
+
+		EventHandler m_Handler;
 	};
 }
