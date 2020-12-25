438b49585e70cad82e18b7aa8eacb8bbc5100106
Jacek <burnertski@gmail.com>
2020-12-25 05:15:21 +0100

    Added Windows window implementation
    The implementation is very basic at the moment

--- Ion/Source/Application/Platform/Windows/WindowsWindow.cpp
+++ Ion/Source/Application/Platform/Windows/WindowsWindow.cpp
@@ -1,14 +1,23 @@
 #include "IonPCH.h"
 
 #include "WindowsWindow.h"
+#include "WindowsApplication.h"
+#include "Event/WindowEvent.h"
+
 #include "Log/Logger.h"
 
 namespace Ion
 {
+	static const CStr _windowNotCreatedMessage = "Cannot {0} before the window is initialized!";
+
+	// Generic Window
+
 	std::shared_ptr<GenericWindow> GenericWindow::Create()
 	{
 		return Ion::WindowsWindow::Create();
 	}
+
+	// Windows Window
 
 	std::shared_ptr<WindowsWindow> WindowsWindow::Create()
 	{
@@ -17,22 +26,158 @@ namespace Ion
 	}
 
 	WindowsWindow::WindowsWindow() :
-		m_HWnd(NULL),
-		m_HDC(NULL)
+		m_HWnd(NULL)
+	{}
+
+	LRESULT WindowsWindow::WindowProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	{
+		WindowsWindow& windowRef = *(WindowsWindow*)GetProp(hWnd, L"WinObj");
+
+		if (uMsg == WM_DESTROY)
+		{
+			PostQuitMessage(0);
+
+			std::shared_ptr<WindowCloseEvent> e = std::make_shared<WindowCloseEvent>(reinterpret_cast<int>(hWnd));
+			windowRef.m_EventCallback(e);
+
+			return 0;
+		}
+
+		switch (uMsg)
+		{
+
+		}
+
+		return DefWindowProc(hWnd, uMsg, wParam, lParam);
 	}
 
-	WindowsWindow::~WindowsWindow()
+	WindowsWindow::~WindowsWindow() {}
+
+	bool WindowsWindow::RegisterWindowClass(HINSTANCE hInstance, LPCWSTR className)
 	{
+		WNDCLASS wc = { };
+
+		if (!m_bRegistered)
+		{
+			wc.style = CS_DBLCLKS;
+			wc.lpfnWndProc = WindowsWindow::WindowProc;
+			wc.cbClsExtra = 0;
+			wc.cbWndExtra = 0;
+			wc.hInstance = hInstance;
+			wc.hIcon = NULL;
+			wc.hCursor = NULL;
+			wc.hbrBackground = NULL;
+			wc.lpszMenuName = NULL;
+			wc.lpszClassName = className;
+
+			if (!RegisterClass(&wc))
+			{
+				MessageBox(NULL, L"Windows WNDCLASS Registration Failed!\nCannot create a window.", L"Error!", MB_ICONEXCLAMATION | MB_OK);
+				return false;
+			}
+
+			m_bRegistered = true;
+			return true;
+		}
+		return false;
 	}
 
-	void WindowsWindow::Initialize()
-	{
+	const wchar* AppClassName = L"IonAppWindow";
 
+	bool WindowsWindow::Initialize()
+	{
+		HINSTANCE hInstance = WindowsApplication::GetHInstance();
+
+		if (!RegisterWindowClass(hInstance, AppClassName)) 
+			return false;
+
+		UINT32 WindowStyleEx = 0;
+		UINT32 WindowStyle = 0;
+
+		WindowStyle |= WS_CAPTION;
+		WindowStyle |= WS_MINIMIZEBOX;
+		WindowStyle |= WS_SYSMENU;
+
+		WindowStyleEx |= WS_EX_APPWINDOW;
+		WindowStyleEx |= WS_EX_WINDOWEDGE;
+
+		int desiredWidth = 1280;
+		int desiredHeight = 720;
+
+		m_HWnd = CreateWindowEx(
+			WindowStyleEx,
+			AppClassName,
+			m_Title.c_str(),
+			WindowStyle,
+
+			CW_USEDEFAULT,
+			CW_USEDEFAULT,
+			desiredWidth,
+			desiredHeight,
+
+			NULL,
+			NULL,
+			hInstance,
+			NULL
+		);
+
+		if (m_HWnd == NULL)
+		{
+			MessageBox(NULL, L"Windows Window Creation Failed!\nCannot create a window.", L"Error!", MB_ICONEXCLAMATION | MB_OK);
+			return false;
+		}
+
+		SetProp(m_HWnd, L"WinObj", this);
+
+		return true;
 	}
 
-	void WindowsWindow::SetTitle(WCStr title)
+	void WindowsWindow::Show()
 	{
+		if (m_HWnd == NULL)
+		{
+			ION_LOG_ENGINE_CRITICAL(_windowNotCreatedMessage, "show the window");
+			return;
+		}
+
+		if (!m_bVisible)
+		{
+			m_bVisible = true;
+
+			int showWindowCmd = SW_SHOW;
+			ShowWindow(m_HWnd, showWindowCmd);
+		}
+	}
+
+	void WindowsWindow::Hide()
+	{
+		if (m_HWnd == NULL)
+		{
+			ION_LOG_ENGINE_CRITICAL(_windowNotCreatedMessage, "hide the window");
+			return;
+		}
+
+		if (m_bVisible)
+		{
+			m_bVisible = false;
+
+			ShowWindow(m_HWnd, SW_HIDE);
+		}
+	}
+
+	void WindowsWindow::SetTitle(const WCStr title)
+	{
+		if (m_HWnd != NULL)
+		{
+			m_Title = title;
+
+			SetWindowText(m_HWnd, m_Title.c_str());
+		}
+	}
+
+	void WindowsWindow::SetEnabled(bool bEnabled)
+	{
+		EnableWindow(m_HWnd, bEnabled);
 	}
 
 	void WindowsWindow::Resize()
@@ -41,9 +186,19 @@ namespace Ion
 
 	bool WindowsWindow::GetDimensions(int& width, int& height) const
 	{
+
 		return false;
 	}
 
+	HDC WindowsWindow::GetHDC() const
+	{
+		if (m_HWnd == NULL)
+		{
+			ION_LOG_ENGINE_CRITICAL(_windowNotCreatedMessage, "get the HDC");
+			return NULL;
+		}
 
+		return GetDC(m_HWnd);
+	}
 }
 
--- Ion/Source/Application/Platform/Windows/WindowsWindow.h
+++ Ion/Source/Application/Platform/Windows/WindowsWindow.h
@@ -4,29 +4,42 @@
 
 namespace Ion
 {
-	class WindowsWindow : public GenericWindow
+	class ION_API WindowsWindow : public GenericWindow
 	{
 	public:
 		static std::shared_ptr<WindowsWindow> Create();
 
 		virtual ~WindowsWindow();
 
-		virtual void Initialize() override;
+		bool RegisterWindowClass(HINSTANCE hInstance, LPCWSTR className);
 
-		virtual void SetTitle(WCStr title) override;
+		virtual bool Initialize() override;
+
+		virtual void Show() override;
+		virtual void Hide() override;
+
+		virtual void SetTitle(const WCStr title) override;
+
+		virtual void SetEnabled(bool bEnabled);
 
 		virtual void Resize() override;
 		virtual bool GetDimensions(int& width, int& height) const override;
 
 		FORCEINLINE HWND GetHWnd() const { return m_HWnd; }
-		FORCEINLINE HDC GetHDC() const { return m_HDC; }
+		HDC GetHDC() const;
 
 	protected:
 		// Protected constructor: Only shared_ptrs of this class can be made.
 		WindowsWindow();
 
+		static LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
 	private:
 		HWND m_HWnd;
-		HDC m_HDC;
+		std::wstring m_Title;
+		bool m_bVisible = false;
+
+		// Should be true after Initialize() has been called.
+		bool m_bRegistered = false;
 	};
 }
