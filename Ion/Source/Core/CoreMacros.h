4e82d88f24ae64ab26c29f177829e9a19043b16d
Jacek <burnertski@gmail.com>
2020-12-29 19:42:48 +0100

    Added Assert macro

--- Ion/Source/Core/CoreMacros.h
+++ Ion/Source/Core/CoreMacros.h
@@ -4,3 +4,20 @@
 #define NODISCARD [[nodiscard]]
 
 #define BITFLAG(x) (1 << (x))
+
+// Debug macros
+
+#ifdef ION_DEBUG
+
+#define ASSERT(x) \
+if (!(x)) \
+{ \
+	ION_LOG_ENGINE_CRITICAL("Assertion failed:\n {0}\n function: {1}\n in {2}:{3}", #x, __FUNCTION__, __FILE__, __LINE__); \
+	__debugbreak(); \
+}
+
+#else
+
+#define ASSERT(x)
+
+#endif
