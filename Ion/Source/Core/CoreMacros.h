4d5942d8029e0670db3b810f4bc6658e5940c7e5
Jacek <burnertski@gmail.com>
2021-01-02 18:38:12 +0100

    Added side independent Logging macros

--- Ion/Source/Core/CoreMacros.h
+++ Ion/Source/Core/CoreMacros.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "Log/Logger.h"
+
 #define FORCEINLINE __forceinline
 #define NODISCARD [[nodiscard]]
 
@@ -9,16 +11,10 @@
 
 #ifdef ION_DEBUG
 
-#ifdef ION_ENGINE
-#define _LOG_CRITICAL(...) ION_LOG_ENGINE_CRITICAL(__VA_ARGS__)
-#else
-#define _LOG_CRITICAL(...) ION_LOG_CRITICAL(__VA_ARGS__)
-#endif
-
 #define ASSERT(x) \
 if (!(x)) \
 { \
-	_LOG_CRITICAL("Assertion failed:\n {0}\n function: {1}\n in {2}:{3}", #x, __FUNCTION__, __FILE__, __LINE__); \
+	LOG_CRITICAL("Assertion failed:\n {0}\n function: {1}\n in {2}:{3}", #x, __FUNCTION__, __FILE__, __LINE__); \
 	__debugbreak(); \
 }
 
--- Ion/Source/Log/Logger.h
+++ Ion/Source/Log/Logger.h
@@ -30,47 +30,55 @@ namespace Ion
 }
 
 #ifdef ION_LOG_ENABLED
+	#ifdef ION_ENGINE
+		#define ION_LOG_ENGINE_TRACE(...)      Ion::Logger::GetEngineLogger()->trace(__VA_ARGS__)
+		#define ION_LOG_ENGINE_DEBUG(...)      Ion::Logger::GetEngineLogger()->debug(__VA_ARGS__)
+		#define ION_LOG_ENGINE_INFO(...)       Ion::Logger::GetEngineLogger()->info(__VA_ARGS__)
+		#define ION_LOG_ENGINE_WARN(...)       Ion::Logger::GetEngineLogger()->warn(__VA_ARGS__)
+		#define ION_LOG_ENGINE_ERROR(...)      Ion::Logger::GetEngineLogger()->error(__VA_ARGS__)
+		#define ION_LOG_ENGINE_CRITICAL(...)   Ion::Logger::GetEngineLogger()->critical(__VA_ARGS__)
 
-#ifdef ION_ENGINE
+		#define ION_LOG_ENGINE_BADPLATFORMFUNCTIONCALL() ION_LOG_ENGINE_CRITICAL("{0} is not supposed to be called on this platform!", __FUNCTION__)
+	#endif
 
-#define ION_LOG_ENGINE_TRACE(...)      Ion::Logger::GetEngineLogger()->trace(__VA_ARGS__)
-#define ION_LOG_ENGINE_DEBUG(...)      Ion::Logger::GetEngineLogger()->debug(__VA_ARGS__)
-#define ION_LOG_ENGINE_INFO(...)       Ion::Logger::GetEngineLogger()->info(__VA_ARGS__)
-#define ION_LOG_ENGINE_WARN(...)       Ion::Logger::GetEngineLogger()->warn(__VA_ARGS__)
-#define ION_LOG_ENGINE_ERROR(...)      Ion::Logger::GetEngineLogger()->error(__VA_ARGS__)
-#define ION_LOG_ENGINE_CRITICAL(...)   Ion::Logger::GetEngineLogger()->critical(__VA_ARGS__)
+	#define ION_LOG_TRACE(...)             Ion::Logger::GetClientLogger()->trace(__VA_ARGS__)
+	#define ION_LOG_DEBUG(...)             Ion::Logger::GetClientLogger()->debug(__VA_ARGS__)
+	#define ION_LOG_INFO(...)              Ion::Logger::GetClientLogger()->info(__VA_ARGS__)
+	#define ION_LOG_WARN(...)              Ion::Logger::GetClientLogger()->warn(__VA_ARGS__)
+	#define ION_LOG_ERROR(...)             Ion::Logger::GetClientLogger()->error(__VA_ARGS__)
+	#define ION_LOG_CRITICAL(...)          Ion::Logger::GetClientLogger()->critical(__VA_ARGS__)
 
-#define ION_LOG_ENGINE_BADPLATFORMFUNCTIONCALL() ION_LOG_ENGINE_CRITICAL("{0} is not supposed to be called on this platform!", __FUNCTION__)
+	// Side independent
 
-#endif
-
-#define ION_LOG_TRACE(...)             Ion::Logger::GetClientLogger()->trace(__VA_ARGS__)
-#define ION_LOG_DEBUG(...)             Ion::Logger::GetClientLogger()->debug(__VA_ARGS__)
-#define ION_LOG_INFO(...)              Ion::Logger::GetClientLogger()->info(__VA_ARGS__)
-#define ION_LOG_WARN(...)              Ion::Logger::GetClientLogger()->warn(__VA_ARGS__)
-#define ION_LOG_ERROR(...)             Ion::Logger::GetClientLogger()->error(__VA_ARGS__)
-#define ION_LOG_CRITICAL(...)          Ion::Logger::GetClientLogger()->critical(__VA_ARGS__)
-
+	#ifdef ION_ENGINE
+		#define LOG_TRACE(...)                 ION_LOG_ENGINE_TRACE(__VA_ARGS__)
+		#define LOG_DEBUG(...)                 ION_LOG_ENGINE_DEBUG(__VA_ARGS__)
+		#define LOG_INFO(...)                  ION_LOG_ENGINE_INFO(__VA_ARGS__)
+		#define LOG_WARN(...)                  ION_LOG_ENGINE_WARN(__VA_ARGS__)
+		#define LOG_ERROR(...)                 ION_LOG_ENGINE_ERROR(__VA_ARGS__)
+		#define LOG_CRITICAL(...)              ION_LOG_ENGINE_CRITICAL(__VA_ARGS__)
+	#else
+		#define LOG_TRACE(...)                 ION_LOG_TRACE(__VA_ARGS__)
+		#define LOG_DEBUG(...)                 ION_LOG_DEBUG(__VA_ARGS__)
+		#define LOG_INFO(...)                  ION_LOG_INFO(__VA_ARGS__)
+		#define LOG_WARN(...)                  ION_LOG_WARN(__VA_ARGS__)
+		#define LOG_ERROR(...)                 ION_LOG_ERROR(__VA_ARGS__)
+		#define LOG_CRITICAL(...)              ION_LOG_CRITICAL(__VA_ARGS__)
+	#endif
 #else
+	#ifdef ION_ENGINE
+		#define ION_LOG_ENGINE_TRACE(...)
+		#define ION_LOG_ENGINE_DEBUG(...)
+		#define ION_LOG_ENGINE_INFO(...)
+		#define ION_LOG_ENGINE_WARN(...)
+		#define ION_LOG_ENGINE_ERROR(...)
+		#define ION_LOG_ENGINE_CRITICAL(...)
+	#endif
 
-#ifdef ION_ENGINE
-
-#define ION_LOG_ENGINE_TRACE(...)
-#define ION_LOG_ENGINE_DEBUG(...)
-#define ION_LOG_ENGINE_INFO(...)
-#define ION_LOG_ENGINE_WARN(...)
-#define ION_LOG_ENGINE_ERROR(...)
-#define ION_LOG_ENGINE_CRITICAL(...)
-
-#define ION_LOG_ENGINE_BADPLATFORMFUNCTIONCALL(...)
-
-#endif
-
-#define ION_LOG_TRACE(...)
-#define ION_LOG_DEBUG(...)
-#define ION_LOG_INFO(...)
-#define ION_LOG_WARN(...)
-#define ION_LOG_ERROR(...)
-#define ION_LOG_CRITICAL(...)
-
+	#define ION_LOG_TRACE(...)
+	#define ION_LOG_DEBUG(...)
+	#define ION_LOG_INFO(...)
+	#define ION_LOG_WARN(...)
+	#define ION_LOG_ERROR(...)
+	#define ION_LOG_CRITICAL(...)
 #endif
