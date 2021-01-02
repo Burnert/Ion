3cc7ea458c9171b3cabb3986c7d279a38aa874a9
Jacek <burnertski@gmail.com>
2021-01-02 18:23:06 +0100

    Temporary log macro fix

--- Ion/Source/Core/CoreMacros.h
+++ Ion/Source/Core/CoreMacros.h
@@ -9,10 +9,16 @@
 
 #ifdef ION_DEBUG
 
+#ifdef ION_ENGINE
+#define _LOG_CRITICAL(...) ION_LOG_ENGINE_CRITICAL(__VA_ARGS__)
+#else
+#define _LOG_CRITICAL(...) ION_LOG_CRITICAL(__VA_ARGS__)
+#endif
+
 #define ASSERT(x) \
 if (!(x)) \
 { \
-	ION_LOG_ENGINE_CRITICAL("Assertion failed:\n {0}\n function: {1}\n in {2}:{3}", #x, __FUNCTION__, __FILE__, __LINE__); \
+	_LOG_CRITICAL("Assertion failed:\n {0}\n function: {1}\n in {2}:{3}", #x, __FUNCTION__, __FILE__, __LINE__); \
 	__debugbreak(); \
 }
 
