5d331febad0c6357a5fd1efca0aceb0b84daec72
Burnert <burnertski@gmail.com>
2021-03-04 14:50:15 +0100

    Update OpenGL.cpp
    Added failure log

--- Ion/Source/OpenGL/OpenGL.cpp
+++ Ion/Source/OpenGL/OpenGL.cpp
@@ -1,7 +1,10 @@
 #include "IonPCH.h"
 
 #include "OpenGl.h"
+
+#ifdef ION_PLATFORM_WINDOWS
 #include "OpenGL/Windows/OpenGLWindows.h"
+#endif
 
 namespace Ion
 {
@@ -9,6 +12,8 @@ namespace Ion
     {
 #ifdef ION_PLATFORM_WINDOWS
         OpenGLWindows::InitOpenGL();
+#else
+        LOG_CRITICAL("OpenGL implementation is not defined on this platform!");
 #endif
     }
 }
