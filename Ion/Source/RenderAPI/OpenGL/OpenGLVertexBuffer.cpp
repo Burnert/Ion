3c3b873453aa43280c20444b497cc6ff4cdad1bb
Burnert <burnertski@gmail.com>
2021-04-05 17:53:47 +0200

    Update OpenGLVertexBuffer.cpp
    Changed glCreateBuffers to glGenBuffers for better backwards compatibility (if it's even needed)

--- Ion/Source/RenderAPI/OpenGL/OpenGLVertexBuffer.cpp
+++ Ion/Source/RenderAPI/OpenGL/OpenGLVertexBuffer.cpp
@@ -6,7 +6,7 @@ namespace Ion
 {
 	OpenGLVertexBuffer::OpenGLVertexBuffer(void* vertices, ulong size)
 	{
-		glCreateBuffers(1, &m_ID);
+		glGenBuffers(1, &m_ID);
 		glBindBuffer(GL_ARRAY_BUFFER, m_ID);
 		glBufferData(GL_ARRAY_BUFFER, size, vertices, GL_STATIC_DRAW);
 	}
