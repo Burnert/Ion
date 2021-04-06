b745468f5eb38c975c8db95918667fef97bf4fee
Burnert <burnertski@gmail.com>
2021-04-06 05:32:18 +0200

    Added Shader Uniform setters

--- Ion/Source/RenderAPI/OpenGL/OpenGLShader.h
+++ Ion/Source/RenderAPI/OpenGL/OpenGLShader.h
@@ -18,6 +18,30 @@ namespace Ion
 		virtual void Bind() const override;
 		virtual void Unbind() const override;
 
+		virtual void SetUniform1f(const std::string& name, float value) const override;
+		virtual void SetUniform2f(const std::string& name, const FVector2& value) const override;
+		virtual void SetUniform3f(const std::string& name, const FVector3& value) const override;
+		virtual void SetUniform4f(const std::string& name, const FVector4& value) const override;
+		virtual void SetUniform1i(const std::string& name, int value) const override;
+		virtual void SetUniform2i(const std::string& name, const IVector2& value) const override;
+		virtual void SetUniform3i(const std::string& name, const IVector3& value) const override;
+		virtual void SetUniform4i(const std::string& name, const IVector4& value) const override;
+		virtual void SetUniform1ui(const std::string& name, uint value) const override;
+		virtual void SetUniform2ui(const std::string& name, const UVector2& value) const override;
+		virtual void SetUniform3ui(const std::string& name, const UVector3& value) const override;
+		virtual void SetUniform4ui(const std::string& name, const UVector4& value) const override;
+		virtual void SetUniformMatrix2f(const std::string& name, const FMatrix2& value) const override;
+		virtual void SetUniformMatrix2x3f(const std::string& name, const FMatrix2x3& value) const override;
+		virtual void SetUniformMatrix2x4f(const std::string& name, const FMatrix2x4& value) const override;
+		virtual void SetUniformMatrix3f(const std::string& name, const FMatrix3& value) const override;
+		virtual void SetUniformMatrix3x2f(const std::string& name, const FMatrix3x2& value) const override;
+		virtual void SetUniformMatrix3x4f(const std::string& name, const FMatrix3x4& value) const override;
+		virtual void SetUniformMatrix4f(const std::string& name, const FMatrix4& value) const override;
+		virtual void SetUniformMatrix4x2f(const std::string& name, const FMatrix4x2& value) const override;
+		virtual void SetUniformMatrix4x3f(const std::string& name, const FMatrix4x3& value) const override;
+
+		int GetUniformLocation(const std::string& name) const;
+
 		FORCEINLINE static constexpr uint ShaderTypeToGLShaderType(EShaderType type)
 		{
 			switch (type)
@@ -35,5 +59,7 @@ namespace Ion
 		uint m_ProgramID;
 		bool m_bCompiled;
 		std::unordered_map<EShaderType, SShaderInfo> m_Shaders;
+
+		mutable std::unordered_map<std::string, int> m_UniformCache;
 	};
 }
--- Ion/Source/Renderer/Shader.h
+++ Ion/Source/Renderer/Shader.h
@@ -30,6 +30,28 @@ namespace Ion
 		virtual void Bind() const = 0;
 		virtual void Unbind() const = 0;
 
+		virtual void SetUniform1f(const std::string& name, float value) const = 0;
+		virtual void SetUniform2f(const std::string& name, const FVector2& value) const = 0;
+		virtual void SetUniform3f(const std::string& name, const FVector3& value) const = 0;
+		virtual void SetUniform4f(const std::string& name, const FVector4& value) const = 0;
+		virtual void SetUniform1i(const std::string& name, int value) const = 0;
+		virtual void SetUniform2i(const std::string& name, const IVector2& value) const = 0;
+		virtual void SetUniform3i(const std::string& name, const IVector3& value) const = 0;
+		virtual void SetUniform4i(const std::string& name, const IVector4& value) const = 0;
+		virtual void SetUniform1ui(const std::string& name, uint value) const = 0;
+		virtual void SetUniform2ui(const std::string& name, const UVector2& value) const = 0;
+		virtual void SetUniform3ui(const std::string& name, const UVector3& value) const = 0;
+		virtual void SetUniform4ui(const std::string& name, const UVector4& value) const = 0;
+		virtual void SetUniformMatrix2f(const std::string& name, const FMatrix2& value) const = 0;
+		virtual void SetUniformMatrix2x3f(const std::string& name, const FMatrix2x3& value) const = 0;
+		virtual void SetUniformMatrix2x4f(const std::string& name, const FMatrix2x4& value) const = 0;
+		virtual void SetUniformMatrix3f(const std::string& name, const FMatrix3& value) const = 0;
+		virtual void SetUniformMatrix3x2f(const std::string& name, const FMatrix3x2& value) const = 0;
+		virtual void SetUniformMatrix3x4f(const std::string& name, const FMatrix3x4& value) const = 0;
+		virtual void SetUniformMatrix4f(const std::string& name, const FMatrix4& value) const = 0;
+		virtual void SetUniformMatrix4x2f(const std::string& name, const FMatrix4x2& value) const = 0;
+		virtual void SetUniformMatrix4x3f(const std::string& name, const FMatrix4x3& value) const = 0;
+
 		FORCEINLINE static constexpr const char* ShaderTypeToString(EShaderType type)
 		{
 			switch (type)
