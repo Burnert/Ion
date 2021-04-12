2a919751a97f8725cc2555130669b6b27696a011
Burnert <burnertski@gmail.com>
2021-04-12 14:56:43 +0200

    Added more template aliases

--- Ion/Source/Core/Templates/StandardWrappers.h
+++ Ion/Source/Core/Templates/StandardWrappers.h
@@ -23,3 +23,45 @@ using TIsIntegral = std::is_integral<T>;
 
 template<typename T>
 inline constexpr bool TIsIntegralV = std::is_integral_v<T>;
+
+// IsSame
+
+template<typename T, typename U>
+using TIsSame = std::is_same<T, U>;
+
+template<typename T, typename U>
+inline constexpr bool TIsSameV = std::is_same_v<T, U>;
+
+// BoolConstant
+
+template<bool val>
+using TBool = std::bool_constant<val>;
+
+// IsAnyOf
+
+template<typename T, typename... Types>
+inline constexpr bool TIsAnyOfV = std::_Is_any_of_v<T, Types...>;
+
+// Disjunction (OR)
+
+template<typename... T>
+using TOr = std::disjunction<T...>;
+
+template<typename... T>
+inline constexpr bool TOrV = std::disjunction_v<T...>;
+
+// Conjunction (AND)
+
+template<typename... T>
+using TAnd = std::conjunction<T...>;
+
+template<typename... T>
+inline constexpr bool TAndV = std::conjunction_v<T...>;
+
+// Negation (NOT)
+
+template<typename T>
+using TNot = std::negation<T>;
+
+template<typename T>
+inline constexpr bool TNotV = std::negation_v<T>;
