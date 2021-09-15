b67cf1dc4538a2af8427859eff897233d5a1499c
Burnert <burnertski@gmail.com>
2021-09-15 02:16:42 +0200

    Update CoreTypes.h
    Added new type names (for better clarity)

--- Ion/Source/Core/CoreTypes.h
+++ Ion/Source/Core/CoreTypes.h
@@ -1,5 +1,7 @@
 #pragma once
 
+// Legacy types
+
 using llong        = int64_t;
 using ubyte        = uint8_t;
 using uchar        = uint8_t;
@@ -7,7 +9,21 @@ using ushort       = uint16_t;
 using uint         = uint32_t;
 using ulong        = unsigned long;
 using ullong       = uint64_t;
-			       
+
+// New types
+
+using int8  = int8_t;
+using int16 = int16_t;
+using int32 = int32_t;
+using int64 = int64_t;
+
+using uint8  = uint8_t;
+using uint16 = uint16_t;
+using uint32 = uint32_t;
+using uint64 = uint64_t;
+
+
+
 using wchar        = wchar_t;
 
 using String       = std::string;
