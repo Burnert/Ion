7cc94cca1997b0586ce10b0b73dd1daa14cd7c0d
Jacek Rogal <burnertski@gmail.com>
2023-01-21 17:43:33 +0100

    Added a new component class

---
+++ Ion/Source/Engine/Components/Component.cpp
@@ -0,0 +1,23 @@
+#include "IonPCH.h"
+
+#include "Component.h"
+
+namespace Ion
+{
+	MComponent::MComponent()
+	{
+	}
+
+	void MComponent::OnCreate()
+	{
+	}
+
+	void MComponent::OnDestroy()
+	{
+	}
+
+	void MComponent::Tick(float deltaTime)
+	{
+		// Empty tick
+	}
+}
---
+++ Ion/Source/Engine/Components/Component.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "Matter/Object.h"
+
+namespace Ion
+{
+	class ION_API MComponent : public MObject
+	{
+		MCLASS(MComponent)
+		using Super = MObject;
+
+	public:
+		MComponent();
+
+		virtual void OnCreate() override;
+		virtual void OnDestroy() override;
+		virtual void Tick(float deltaTime) override;
+
+		
+
+	private:
+	};
+}
